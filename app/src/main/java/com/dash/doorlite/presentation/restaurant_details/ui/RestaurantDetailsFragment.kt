b0969551adb3a59dba20b50b03a74854568ff20d
Udyatbhanu <udyatbhanu@gmail.com>
2021-03-15 12:33:36 -0700

    delivery status

--- app/src/main/java/com/dash/doorlite/presentation/restaurant_details/ui/RestaurantDetailsFragment.kt
+++ app/src/main/java/com/dash/doorlite/presentation/restaurant_details/ui/RestaurantDetailsFragment.kt
@@ -106,7 +106,6 @@ class RestaurantDetailsFragment : Fragment() {
         viewModel.state.observe(viewLifecycleOwner) {
             when (it) {
                 is RestaurantsDetailsFragmentState.RestaurantDetails -> {
-                    println("got tags ${it.details.tags}")
                     binding.details = it.details
                 }
 
